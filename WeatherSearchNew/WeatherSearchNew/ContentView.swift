eb01e29cc15ec21a679d31a7acf1a3b27894871a
AlessandroLima <aletlima@gmail.com>
2024-04-14 07:20:58 -0300

    feat: change WeatherFramework to Weather'

--- WeatherSearchNew/WeatherSearchNew/ContentView.swift
+++ WeatherSearchNew/WeatherSearchNew/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import WeatherFramework
+import Weather
 
 struct ContentView: View {
     
--- WeatherSearchNew/WeatherSearchNew/WeatherSearchNewApp.swift
+++ WeatherSearchNew/WeatherSearchNew/WeatherSearchNewApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import WeatherFramework
+import Weather
 
 @main
 struct WeatherSearchNewApp: App {
