3a1bb55f1df047b445ac89ba6930a190c4a2ab83
AlessandroLima <aletlima@gmail.com>
2024-04-14 13:05:58 -0300

    feat: old way

--- Weather/README.md
+++
@@ -1,3 +0,0 @@
-# Weather
-
-A description of this package.
---
+++ Weather/Weather/Weather.h
@@ -0,0 +1,18 @@
+//
+//  Weather.h
+//  Weather
+//
+//  Created by Alessandro Teixeira Lima on 14/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Weather.
+FOUNDATION_EXPORT double WeatherVersionNumber;
+
+//! Project version string for Weather.
+FOUNDATION_EXPORT const unsigned char WeatherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Weather/PublicHeader.h>
+
+
